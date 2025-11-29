7310874a928289a0706a02b2a6a1abc82e8f6a32
arjun-sapkota887 <asapkota@upei.ca>
2025-11-29 00:34:48 -0400

    Delete project directory

--- project/src/train_baselines.py
+++
@@ -1,4 +0,0 @@
-from data import load_data
-from features import create_features
-from evaluate import classification_metrics, regression_metrics, plot_confusion_matrix, plot_residuals
-from utils import ensure_dirs, set_seed
